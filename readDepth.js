84656adf05d3078d0b3f77336d7dc1996384c878
Timothy Pfafman <tim@pfafman.com>
2022-10-23 15:06:09 -0600

    merge

--- readDepth.js
+++ readDepth.js
@@ -10,15 +10,15 @@ checkSum = (data) => {
     return (data[0] + data[1] + data[2]) & 0x00ff
 }
 
-const delay = (ms) => {
-    new Promise(resolve => setTimeout(resolve, ms));
+delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-raspi.init(() => {
+async raspi.init(() => {
 
     var serial = new Serial({portId: "/dev/serial0", baudRate: 9600});
-    serial.open(() => {
-        serial.on('data', (byte) => {
+    async serial.open(() => {
+        async serial.on('data', (byte) => {
             i++;
             data.push(byte[0])
             //console.log("Data:[", byte.length, ']:',i, ':', byte[0]);
